1d35e6b84f37db7925942eaaa29fe4abbe95e8cd
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-14 15:56:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-    lowest = 
+    lowest = name_hash[0].to_i
     name_hash.each do |item, price|
       if price < lowest
         lowest = price
