f4861fe3acb1ab6a34b097d77f14c45624e31ed9
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-14 15:26:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 
 # prereqs: iterators, hashes, conditional logic
 # # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
+# require 'pry'
 def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
@@ -11,7 +11,6 @@ def key_for_min_value(name_hash)
           lowest = price
       if price < lowest
         lowest == price
-        binding.pry
       end
     end
     lowest
