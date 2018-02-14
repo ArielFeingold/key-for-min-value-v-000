732f8741ae0cbdff76b0936e79394192bac4f667
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-14 15:21:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
+# # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil
+  else
+    lowest = nil
+    name_hash.each.with_index do |item, price, i|
+          lowest = price
+      if price < lowest
+        lowest == price
+        binding.pry
+      end
+    end
+    lowest
+  end
 end
